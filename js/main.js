9b1b7990278ffaf27b17489006c3d6e045897b60
Juan Pablo <larrazajuanp@gmail.com>
2020-11-02 16:21:19 -0300

    Habia agregado un main.js pero al final no lo voy a utilizar js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Variables
+const eightBit = document.querySelector('.viajandoSiempre');
+const overlay = document.querySelector('.overlay');
+const imgViajando = document.getElementById('viajandoSiempre');
+
+eightBit.addEventListener('mouseover', (e) => {
+    //overlay.style.display = 'none';
+    console.log(e.target)
+    imgViajando.style.opacity = 0.5;
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+        <div class="overlay">
+            <h1>Visitar el sitio</h1>
+        </div>
+    `
+    eightBit.appendChild(div)
+
+    eightBit.appendChild('mouseout', () => {
+        imgViajando.style.opacity = 1;
+    })
+
+})
